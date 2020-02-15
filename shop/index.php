95a4ccfef7cc52b608db991768c42844fa3ba6b0
phpwebdevelope <36544765+phpwebdevelope@users.noreply.github.com>
2020-02-15 17:21:43 -0500

    Update index.php

--- shop/index.php
+++ shop/index.php
@@ -1,1 +1,10 @@
-Coming Soon
+ <section id="shop">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">About</h2>
+                    <h3 class="section-subheading text-muted">Coming Soon.</h3>
+                </div>
+            </div>
+   </div>
+</section>
